86fbd06c46372e5856fca0874696e596dbe36676
ChanuPerera <chanukaperera53@gmail.com>
2023-06-06 12:18:16 +0530

    Radial Background Final

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -3,6 +3,7 @@ import {View, Text, StatusBar, Dimensions, StyleSheet} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import {BlurView} from '@react-native-community/blur';
 import tw from 'tailwind-react-native-classnames';
+import RadialGradient from 'react-native-radial-gradient';
 
 const Home = () => {
   const screenWidth = Dimensions.get('window').width;
@@ -18,14 +19,33 @@ const Home = () => {
         <StatusBar translucent backgroundColor="transparent" />
       </LinearGradient>
 
-      <View className="bg-[#171738] h-full justify-center items-center flex">
-        <View
-          className={'bg-[#A682FF] rounded-full'}
-          style={{width: circleWidth, height: circleWidth}}
-        />
+      <View className="bg-[#171738] h-full w-full justify-center items-center text-center" >
+        <RadialGradient
+          colors={['#A682FF', '#171738']}
+          style={styles.gradient}
+          stops={[0.1, 0.9]}
+          center={[0.3,0.5, 0.3]}
+          radius={200}
+          >
+          
+        </RadialGradient>
       </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  gradient: {
+    
+    width: '100%',
+    height: '100%',
+    opacity: 0.4,
+  },
+});
 
 export default Home;
